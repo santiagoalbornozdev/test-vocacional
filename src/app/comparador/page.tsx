dd68c356c3082506468c9bebadabd411b6ae4199
Santiago Albornoz <albornozsantiago358@gmail.com>
2026-04-23 05:46:16 -0300

    solucionando error de page vacios, impedian deploy en vercel

---
+++ src/app/comparador/page.tsx
@@ -0,0 +1,8 @@
+export default function ComparadorPage() {
+    return (
+      <div className="p-8">
+        <h1 className="text-2xl font-bold">Comparador de Carreras</h1>
+        <p>Próximamente...</p>
+      </div>
+    );
+  }
---
+++ src/app/opinion/page.tsx
@@ -0,0 +1,8 @@
+export default function ComparadorPage() {
+    return (
+      <div className="p-8">
+        <h1 className="text-2xl font-bold">Opinion</h1>
+        <p>Próximamente...</p>
+      </div>
+    );
+  }
